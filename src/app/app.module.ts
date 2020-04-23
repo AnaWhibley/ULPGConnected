16d0e79097ffa6c793688758c218abdfac5d0a57
AnaWhibley <anawhibley@gmail.com>
2020-04-23 15:55:40 +0100

    Showing Login and Sign In from main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { IndexComponent } from './index/index.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    IndexComponent
+    IndexComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
