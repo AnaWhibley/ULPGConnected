64e88795150d83b9a2543cf3d52584da749149cb
Adrián Godoy Martínez <adriangodoymartinez@icloud.com>
2020-04-23 17:30:05 +0100

    Environment and module modified
    Environment and module modified in order to add  firebase configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { IndexComponent } from './index/index.component';
 import { SigninComponent } from './signin/signin.component';
+import {AngularFireModule} from "@angular/fire";
+import {AngularFireAuthModule} from "@angular/fire/auth";
+import {environment} from "../environments/environment";
+import {AngularFirestoreModule} from "@angular/fire/firestore";
+import {AngularFireStorage} from "@angular/fire/storage";
 
 @NgModule({
   declarations: [
@@ -16,9 +20,12 @@ import { SigninComponent } from './signin/signin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
-  providers: [],
+  providers: [AngularFireStorage],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCaw6uY77XgWBlth9_yq9HpuJw0oFcDTkU",
+    authDomain: "ulpgconnected.firebaseapp.com",
+    databaseURL: "https://ulpgconnected.firebaseio.com",
+    projectId: "ulpgconnected",
+    storageBucket: "ulpgconnected.appspot.com",
+    messagingSenderId: "403026159195",
+    appId: "1:403026159195:web:3a55c78cb598842d99c17e"
+  }
 };
 
 /*
