8334bdefcebbeb23520a5b7228c2fd8a1408489d
Adrián Godoy Martínez <adriangodoymartinez@icloud.com>
2020-05-08 13:31:02 +0100

    Method to get all the post from firebase finished

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {AngularFirestore} from "@angular/fire/firestore";
+import {Router} from "@angular/router";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
+
+  public get posts(): Observable<any> {
+    return this.db.collection('posts').valueChanges({ idField: 'propertyId' });
+  }
+
 }
