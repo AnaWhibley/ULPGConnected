c0a52438bdd768cecfdcab5be9935c610a0661aa
AnaWhibley <anawhibley@gmail.com>
2020-05-12 15:46:18 +0100

    Finished simple styling

--- src/app/likes/likes.component.ts
+++ src/app/likes/likes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-likes',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LikesComponent implements OnInit {
 
+
+  @Input() readonly: boolean = false;
+  private clicked: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  clickEvent() {
+    this.clicked = !this.clicked;
   }
 
 }
