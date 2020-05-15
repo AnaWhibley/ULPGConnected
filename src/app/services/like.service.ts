9eb21be1807b2f738f49f69099aa5f90c6f34850
Adrián Godoy Martínez <adriangodoy199@gmail.com>
2020-05-14 18:15:55 -0700

    Method to get all the likes from firebase

--- src/app/services/like.service.ts
+++ src/app/services/like.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {AngularFirestore} from "@angular/fire/firestore";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LikeService {
 
-  constructor() { }
+  constructor(private db: AngularFirestore, private router: Router) { }
+
+  public get likes(): Observable<any> {
+    return this.db.collection('likes').valueChanges({idField: 'propertyId'});
+  }
+
+
 }
