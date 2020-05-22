52021b8e0cf2fa975c4bedac513bd2bb3f944309
AnaWhibley <anawhibley@gmail.com>
2020-05-22 10:30:00 +0100

    Implemented behaviour for reporting posts

--- src/app/report-menu/report-menu.component.ts
+++ src/app/report-menu/report-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-report-menu',
@@ -9,7 +9,14 @@ export class ReportMenuComponent implements OnInit {
 
   constructor() { }
 
+  @Input() propertyId: string;
+  @Input() userId: string;
+  @Input() postId: string;
+
   ngOnInit() {
   }
 
+  reportHandling() {
+    console.log("!!!!!", this.userId, " - ", this.postId);
+  }
 }
