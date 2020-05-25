12a252d5f4d65ae7e373d99f19a4c51a4bc6b667
AnaWhibley <anawhibley@gmail.com>
2020-05-25 15:30:00 +0100

    Created component for reported messages

---
+++ src/app/reported-posts/reported-posts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reported-posts',
+  templateUrl: './reported-posts.component.html',
+  styleUrls: ['./reported-posts.component.scss']
+})
+export class ReportedPostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
