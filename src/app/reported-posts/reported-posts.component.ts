b9803d67e881a0a711311691aba36f75d3cf9391
AnaWhibley <anawhibley@gmail.com>
2020-05-26 11:58:14 +0100

    Styled reported posts for admins

--- src/app/reported-posts/reported-posts.component.ts
+++ src/app/reported-posts/reported-posts.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ReportService} from '../services/report.service';
 import {PostService} from '../services/post.service';
+import {Router} from '@angular/router';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-reported-posts',
@@ -11,23 +13,45 @@ export class ReportedPostsComponent implements OnInit {
 
   private reports : any;
   private postsReported : Array<any> = [];
+  private loguedUser: any;
+
 
   constructor( private reportService: ReportService,
-               private postService: PostService) { }
+               private postService: PostService,
+               private router: Router,
+               private authService: AuthService) { }
 
   ngOnInit() {
     this.reportService.reports.subscribe(reports => {
       this.reports = reports;
       this.reports.forEach(report => {
         this.postService.getPostById(report.postId).subscribe( element => {
-          if (element !== null) {
+          if (element !== undefined) {
             this.postsReported.push(element);
-            console.log("!!!!!!!",this.postsReported);
+            console.log(this.postsReported);
           }
           }
         );
       });
     });
+
+    this.authService.userStatusChanges
+      .subscribe( userDetails => {
+        this.loguedUser = userDetails;
+      });
+  }
+
+  goToDetails(id){
+    id ? this.router.navigate(['/post-details', id]) : "";
+  }
+
+  goToUserDetails(userId: string){
+
+    if (userId == this.loguedUser.id){
+      this.router.navigate(['/me']);
+    } else {
+      userId ? this.router.navigate(['/user-info', userId]) : "";
+    }
   }
 
 }
