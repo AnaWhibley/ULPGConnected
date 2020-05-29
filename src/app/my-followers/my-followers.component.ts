219a80dfb506ef87bf34098d96ac5e379c112cb8
Ruimán <ruimansanchezmartel@gmail.com>
2020-05-29 09:33:36 +0100

    Add in navbar Add in routing

--- src/app/my-followers/my-followers.component.ts
+++ src/app/my-followers/my-followers.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-my-followers',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyFollowersComponent implements OnInit {
 
-  constructor() { }
+  me: any;
+  subs: Subscription;
+  nFollowers: number;
+  followers = [];
 
-  ngOnInit() {
+  constructor(private authService: AuthService) {
+    this.nFollowers = 0;
   }
 
+  ngOnInit() {
+
+    this.subs = this.authService.userStatusChanges
+      .subscribe( userDetails => {
+        this.me = userDetails;
+        this.nFollowers = this.me.followers.length;
+        this.followers = this.me.followers;
+      });
+  }
 }
