ecb2020c5030fff00908c13ceaf95a80ff475b7f
Ruimán <ruimansanchezmartel@gmail.com>
2020-05-29 13:20:50 +0100

    Corrections

--- src/app/my-followings/my-followings.component.ts
+++ src/app/my-followings/my-followings.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import {Router} from "@angular/router";
 import { Subscription } from 'rxjs';
+import {UserService} from "../services/user.service";
 
 @Component({
   selector: 'app-my-followings',
@@ -13,20 +15,39 @@ export class MyFollowingsComponent implements OnInit {
   subs: Subscription;
   nFollowing: number;
   followings = [];
+  private loguedUser: any;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,
+              private router: Router,
+              private userService: UserService) {
     this.nFollowing = 0;
   }
 
   ngOnInit() {
-
     this.subs = this.authService.userStatusChanges
       .subscribe( userDetails => {
+        this.followings = [];
         this.me = userDetails;
         this.nFollowing = this.me.following.length;
-        this.followings = this.me.following;
-        console.log("!!", this.me, );
-        console.log("followings!!", this.followings);
+        this.me.following.forEach(following => {
+          this.userService.getUserByOtherId(following).subscribe((userData) => {
+            this.followings.push(userData);
+          })
+        });
       });
+
+    this.authService.userStatusChanges
+      .subscribe( userDetails => {
+        this.loguedUser = userDetails;
+      });
+  }
+
+  goToUserDetails(userId: string) {
+
+    if (userId == this.loguedUser.id){
+      this.router.navigate(['/me']);
+    } else {
+      userId ? this.router.navigate(['/user-info', userId]) : "";
+    }
   }
 }
