1e716e2d3d08c6b88bf848e792be6cf87e0e4226
Ruimán <ruimansanchezmartel@gmail.com>
2020-05-29 11:33:46 +0100

    Working

---
+++ src/app/user-item/user-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-user-item',
+  templateUrl: './user-item.component.html',
+  styleUrls: ['./user-item.component.scss']
+})
+export class UserItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
